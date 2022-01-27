4a13711ce72a6b0f64a3773c5d7c6ab5f44bc3ad
Eugene Gogolev <stavrwalker@gmail.com>
2022-01-27 23:51:13 +0300

    important

---
+++ pages/game/[gameId]/turn[turnId].tsx
@@ -0,0 +1,7 @@
+//
+// import { useRouter } from 'next/router'
+// import {NextPage} from "next";
+//
+// const Page: NextPage = () => {
+//     const router = useRouter()
+//     const { pid } = router.query
