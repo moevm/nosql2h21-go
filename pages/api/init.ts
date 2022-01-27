025d80f6fc22363e2abef409adf475b5d09eb537
Eugene Gogolev <stavrwalker@gmail.com>
2022-01-28 00:01:15 +0300

    fixed init

--- pages/api/init.ts
+++ pages/api/init.ts
@@ -9,5 +9,23 @@ type Data = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-   
+    const fileObjs = fs.readdirSync("dataset/", { withFileTypes: true });
+    const promises = fileObjs.map(async (file: any) => {
+        const path = "dataset/" + file.name;
+        const example = fs.readFileSync(path, { encoding: "utf8" }); // string (.sgf)
+        // TODO broken encoding
+        const collection = sgf.parse(example); // js object
+        const collectionSGF = sgf.generate(collection); // string (.sgf)
+
+        const matchData = collection.gameTrees[0].nodes;
+        const metaData = matchData[0];
+        const turnsData = matchData.slice(1);
+
+        
+        
+    });
+
+    await Promise.all(promises);
+
+    res.status(200).json({ result: "ok" });
 }
