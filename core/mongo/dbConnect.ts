b3e21c2d3f6d20954394bb5ad5ee729344426f28
caxapexac <caxapexac@gmail.com>
2022-01-27 23:46:58 +0300

    Added mongo api

---
+++ core/mongo/dbConnect.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const connection: { isConnected: mongoose.Connection["readyState"] } = { isConnected: 0 };
+
+async function dbConnect() {
+    if (connection.isConnected) return;
+
+    const db = await mongoose.connect("mongodb://mongodb:27017/go_db");
+    // const db = await mongoose.connect("mongodb://localhost:27017/go_db");
+        // process.env.DB_HOST!,
+        // const db = await mongoose.connect("mongodb://localhost:27017/NoSQL_t1",
+        // const db = await mongoose.connect(process.env.DB_HOST!,
+
+    // db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+    connection.isConnected = db.connections[0].readyState;
+}
+
+export default dbConnect;
