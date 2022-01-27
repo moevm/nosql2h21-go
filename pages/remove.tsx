244e45441a2a1218a296038cfafc9c318bc0a6bc
Anstberd <43109601+Anstberd@users.noreply.github.com>
2022-01-28 00:16:23 +0300

    Create remove.tsx

---
+++ pages/remove.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Button from '@mui/material/Button';
+import MatchesTable from 'components/MatchesTable';
+import { Grid } from '@mui/material';
+
+const Home: NextPage = () => {
+  return (
+    <Grid container direction="column" alignItems="center" width="100%" >
+      
+    </Grid >
+  )
+}
+
+export default Home
